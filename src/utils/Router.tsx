751b842f03c7b60813c44daecf06e26dc0587bc4
BeVll <vladixerplay@gmail.com>
2024-08-17 13:32:28 +0200

    currencies

--- src/utils/Router.tsx
+++ src/utils/Router.tsx
@@ -3,6 +3,7 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import WeatherPage from "../pages/WeatherPage.tsx";
 import PlayerPage from "../pages/PlayerPage.tsx";
 import HomePage from "../pages/HomePage.tsx";
+import CurrenciesPage from "../pages/CurrenciesPage.tsx";
 
 const Router = () => {
     return (
@@ -11,6 +12,7 @@ const Router = () => {
                     <Route path={"/"} element={<HomePage/>}/>
                     <Route path={"/weather"} element={<WeatherPage/>}/>
                     <Route path={"/player"} element={<PlayerPage/>}/>
+                    <Route path={"/currency"} element={<CurrenciesPage/>}/>
                 </Routes>
         </BrowserRouter>
     );
